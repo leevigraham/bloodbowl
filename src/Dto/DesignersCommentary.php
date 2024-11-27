0da4c98ab7768168c463e8cf88c66d00b70030fd
Leevi Graham <leevi@newism.com.au>
2024-11-28 08:49:21 +1100

    Added Rule Book
    - Added Rule Book
    - Rewrote templates
    - Rewrote CSS

---
+++ src/Dto/DesignersCommentary.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Dto;
+
+class DesignersCommentary
+{
+    public function __construct(
+        public string $source,
+        public string $documentRef,
+        public int $pageRef,
+        public string $question,
+        public string $answer,
+        public ?string $team = null,
+        public ?string $starPlayer = null,
+        public ?string $skill = null,
+        public ?string $trait = null,
+    ) {
+    }
+}
